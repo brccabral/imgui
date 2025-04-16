3fd82dbb6b5a2c2f482515dc9627910de2e3aa50
Bruno Cabral <cabral.brc@gmail.com>
2025-04-16 16:09:38 -0700

    Dawn as installed library

--- backends/cmake/ConfigureBackends.cmake
+++ backends/cmake/ConfigureBackends.cmake
@@ -260,7 +260,7 @@ endif ()
 if ("glfw_wgpu_dawn" IN_LIST SUPPORTED_BACKENDS)
     set(HEADER_FILES imgui_impl_glfw.h imgui_impl_wgpu.h)
     set(CPP_FILES imgui_impl_glfw.cpp imgui_impl_wgpu.cpp)
-    set(TARGET_LIBS webgpu_dawn webgpu_cpp webgpu_glfw glfw)
+    set(TARGET_LIBS dawn::webgpu_dawn)
 
     add_backend("glfw_wgpu_dawn" "${HEADER_FILES}" "${CPP_FILES}" "${TARGET_LIBS}")
 
