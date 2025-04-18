7fc0a5376baf5b1f86d087badae8c47883d08ef4
Bruno Cabral <cabral.brc@gmail.com>
2025-04-18 13:12:55 -0700

    example_win32_directx11

--- backends/cmake/ConfigureBackends.cmake
+++ backends/cmake/ConfigureBackends.cmake
@@ -409,3 +409,13 @@ if ("win32_directx10" IN_LIST SUPPORTED_BACKENDS)
     target_compile_options(imgui_backend_win32_directx10 PUBLIC /nologo /utf-8 /DUNICODE /D_UNICODE)
     target_link_directories(imgui_backend_win32_directx10 PUBLIC ${IMGUI_DIRECTX_10_X86_LIB_DIR})
 endif ()
+
+if ("win32_directx11" IN_LIST SUPPORTED_BACKENDS)
+    set(HEADER_FILES imgui_impl_dx11.h imgui_impl_win32.h)
+    set(CPP_FILES imgui_impl_dx11.cpp imgui_impl_win32.cpp)
+    set(TARGET_LIBS ${IMGUI_DIRECTX_11_X86_LIBRARIES})
+
+    add_backend("win32_directx11" "${HEADER_FILES}" "${CPP_FILES}" "${TARGET_LIBS}")
+    target_compile_options(imgui_backend_win32_directx11 PUBLIC /nologo /utf-8 /DUNICODE /D_UNICODE)
+    target_link_directories(imgui_backend_win32_directx11 PUBLIC ${IMGUI_DIRECTX_11_X86_LIB_DIR})
+endif ()
